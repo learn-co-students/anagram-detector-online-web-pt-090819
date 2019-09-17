faedbafa73009bb7354a855d53bd2b31e4f1723d
Darrel Castellano <dcastellano2007@gmail.com>
2019-09-16 21:48:57 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :string
+  def initialize(string)
+    @string = string
+  end
+
+  def match(array)
+
+    array.select do |word|
+      word.split("").sort == @string.split("").sort
+    end
+    
+  end
+end
