62b7e8579fdef566cda402001b13f3eb83facc69
Seung Kim <seunglives@hotmail.com>
2019-10-05 21:54:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word 
+
+def initialize(word)
+  @word = word
+end
+
+def match(word)
+  word.select do |string|
+      (@word.split("").sort) == (string.split("").sort)
+    end
+  end
+
+end
