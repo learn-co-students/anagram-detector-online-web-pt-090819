c4248ea34cec943c6863079ea9e135224ecf1aaf
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-05 01:56:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select(){|match|
+      match if @word.split("").sort == match.split("").sort
+    }
+  end
+end
