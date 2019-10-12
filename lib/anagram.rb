74782b0bd5617c8482798ec80220a86d26e8eba2
cedric e goddard <8ceg6@comcast.net>
2019-10-12 17:10:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :aw 
+
+  def initialize(aw)
+    @aw= aw
+  end 
+  
+  def match(words)
+    words.find_all do |word|
+      if word.split("").sort == self.aw.split("").sort
+        word
+      end  
+       end         
+  end
+
+
+end  
